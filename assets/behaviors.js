daae324d9a8047c7ecce0742b38ac440ae5168a2
Andrew Hedges <andrew@hedges.name>
2019-11-21 19:43:07 -0800

    Basic slideshow

---
+++ assets/behaviors.js
@@ -0,0 +1,6 @@
+import initSlideshow from './slideshow.js'
+import slideData from './slidedata.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+    initSlideshow(slideData)
+})
---
+++ assets/slidedata.js
@@ -0,0 +1,17 @@
+/*
+Data formats:
+    Image slides: { src: '0010-IMG_6569.jpg' },
+    Video slides: { video: '0910-IMG_1514.mp4' },
+    Title slides: { url: '0010-IMG_6569.jpg', h1: 'Day 1', h2: 'Portland to Moab' },
+*/
+let slideData = [
+    { url: 'barn.jpg', h1: 'Look, ma, no frameworks!', h2: 'Building stuff, the old skool way' },
+    { url: 'barn.jpg', h1: 'Hello, I’m Andrew.', h2: 'I work at Zapier. You wouldn’t guess it, but I’m actually pretty good at imitating animal sounds.' },
+    { src: 'me-dad-sis.jpg' },
+    { src: 'backlit-jeep.jpg' },
+    { src: 'butte.jpg' },
+    { src: 'dino-prints.jpg' },
+    { src: 'future-fossils.jpg' },
+}
+
+export default slideData
