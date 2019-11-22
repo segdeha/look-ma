01abe912db3bb2e563b2c9c3b6b3fe4610e08443
Andrew Hedges <andrew@hedges.name>
2019-11-21 19:47:47 -0800

    Tweak some styles

--- assets/slidedata.js
+++ assets/slidedata.js
@@ -5,7 +5,7 @@ Data formats:
     Title slides: { url: '0010-IMG_6569.jpg', h1: 'Day 1', h2: 'Portland to Moab' },
 */
 let slideData = [
-    { url: 'barn.jpg', h1: 'Look, ma, no frameworks!', h2: 'Building stuff, the old skool way' },
+    { url: 'barn.jpg', h1: 'Look ma,<br>no frameworks!', h2: 'Building stuff, the old skool way' },
     { url: 'barn.jpg', h1: 'Hello, I’m Andrew.', h2: 'I work at Zapier. You wouldn’t guess it, but I’m actually pretty good at imitating animal sounds.' },
     { src: 'me-dad-sis.jpg' },
     { src: 'backlit-jeep.jpg' },
