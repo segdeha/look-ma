c28814c6d39e42044a4c0b7879bece00c7d8966a
Andrew Hedges <andrew@hedges.name>
2019-11-21 19:50:41 -0800

    Update a couple slides and the page title

--- assets/slidedata.js
+++ assets/slidedata.js
@@ -6,11 +6,12 @@ Data formats:
 */
 let slideData = [
     { url: 'barn.jpg', h1: 'Look ma,<br>no frameworks!', h2: 'Building stuff, the old skool way' },
-    { url: 'barn.jpg', h1: 'Hello, I’m Andrew.', h2: 'I work at Zapier. You wouldn’t guess it, but I’m actually pretty good at imitating animal sounds.' },
+    { url: 'stonehenge.jpg', h1: 'Hello, I’m Andrew.', h2: 'I work at Zapier. You wouldn’t guess it, but I’m actually pretty good at imitating animal sounds.' },
     { src: 'me-dad-sis.jpg' },
     { src: 'backlit-jeep.jpg' },
     { src: 'butte.jpg' },
     { src: 'dino-prints.jpg' },
+    { src: 'lizard.jpg' },
     { src: 'future-fossils.jpg' },
 ]
 
